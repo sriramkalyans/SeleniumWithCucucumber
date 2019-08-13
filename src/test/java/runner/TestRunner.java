ef2378d6494994a09ff3578d7e10e92a80f2b45b
Sriram <43890061+sriramkalyans@users.noreply.github.com>
2019-08-13 15:41:38 -0400

    Update TestRunner.java
    testing  jenkins integration

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -3,14 +3,13 @@ package runner;
 import cucumber.api.CucumberOptions;
 import cucumber.api.testng.AbstractTestNGCucumberTests;
 
-
 /**
  * Created by Karthik on 31/01/2019.
  */
 
 //@RunWith(Cucumber.class)
 @CucumberOptions(features = {"src/test/java/features"} , format = {"json:target/cucumber.json","html:target/site/cucumber-pretty"},
-        glue = "steps")
+        glue = "steps") 
 public class TestRunner extends AbstractTestNGCucumberTests{
 
 }
